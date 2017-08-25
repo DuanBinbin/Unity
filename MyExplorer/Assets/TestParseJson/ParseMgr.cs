81bb894b54ba3e2feb32f1d9776a0148286e688a
duan bin <binbin.duan@pisofttech.com>
2017-08-25 21:33:55 +0800

    提交XMLJson解析工具类

---
+++ MyExplorer/Assets/TestParseJson/ParseMgr.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebItem
+{
+    public string name;
+    public string url;
+}
+
+public class ParseMgr : MonoBehaviour {
+
+    public string URL = "D:/USBFile/test.json";
+
+    // Use this for initialization
+    void Start ()
+    {
+        List<Hashtable> list = XmlJsonParseUtiliy.ParserJson(URL);
+        foreach (var item in list)
+        {
+            Debug.LogFormat(" name = {0},url = {0}", item["name"], item["url"]);
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ MyExplorer/Assets/TestParseJson/XmlJsonParseUtiliy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class XmlJsonParseUtiliy
+{
+    public static List<Hashtable> ParserJson(string url)
+    {
+        List<Hashtable> htLists = new List<Hashtable>();
+
+        FileInfo file = new FileInfo(url);
+        StreamReader sr = new StreamReader(file.OpenRead(), Encoding.UTF8);
+        string content = sr.ReadToEnd();
+        sr.Close();
+        sr.Dispose();
+
+        // use MiniJson to let Json to string 
+        object data = MiniJSON.jsonDecode(content) as object;
+
+        ArrayList list = data as ArrayList;
+        if (list != null)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                Hashtable ht = list[i] as Hashtable;
+                htLists.Add(ht);
+            }
+        }
+        else
+        {
+            Debug.Log("MiniJson parse failure");
+        }
+        return htLists;
+    }
+
+}
